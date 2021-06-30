a8b6e98a35a41ad0b92c8ccc235e4b57306099e2
tomastoloza <tomasezequieltoloza@gmail.com>
2021-06-29 23:56:27 -0300

    Agregados errores

--- api/routes/materias.js
+++ api/routes/materias.js
@@ -33,12 +33,12 @@ router.post("/", (req, res) => {
         })
         .then(materia => res.status(201).send({id: materia.id}))
         .catch(error => {
-            if (error === "SequelizeUniqueConstraintError: Validation error") {
-                res.status(400).send('Bad request: existe otra materia con el mismo nombre')
-            } else {
                 console.log(`Error al intentar insertar en la base de datos: ${error}`)
-                res.sendStatus(500)
-            }
+                res.status(500).send({
+                    message: "Bad request",
+                    errorType: error.name,
+                    errorImage: "https://http.cat/500"
+                })
         });
 });
 
@@ -72,12 +72,11 @@ router.put("/:id", (req, res) => {
             }, {fields: ["nombre", "carreraId"]})
             .then(() => res.sendStatus(200))
             .catch(error => {
-                if (error === "SequelizeUniqueConstraintError: Validation error") {
-                    res.status(400).send('Bad request: existe otra materia con el mismo nombre')
-                } else {
-                    console.log(`Error al intentar actualizar la base de datos: ${error}`)
-                    res.sendStatus(500)
-                }
+                res.status(500).send({
+                    message: "Bad request",
+                    errorType: error.name,
+                    errorImage: "https://http.cat/500"
+                })
             });
     findMateria(req.params.id, {
         onSuccess,
